7df3abc12b58d05444aa71686a27ad7d399af8c2
apieper <adrpieper@gmail.com>
2016-09-03 21:02:41 +0200

    new file Interface

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 /**
  * Created by adrian on 03.09.16.
  */
 public class Interface {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Witamy w sumatorze dwóch liczb! Podaj pierwszą liczbę: ");
+        double a = scanner.nextDouble();
+        System.out.println("Podaj drugą liczbę: ");
+        double b = scanner.nextDouble();
+        Calculator calculator = new Calculator();
 
-
+        System.out.println("Suma wynosi: "+calculator.sum(a,b));
+    }
 }
